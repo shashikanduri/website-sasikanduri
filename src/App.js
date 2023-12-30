086aad2b644eb18d436339e4259838caa2ccc488
shashi <shashi.kanduri@outlook.com>
2023-12-30 18:06:43 +0530

    test

--- src/App.js
+++ src/App.js
@@ -8,21 +8,16 @@ import Navbar from "./components/Navbar";
 
 function App() {
 
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    navigate('/');
-  }, []);
-
   return (
     <div className="App">
       <div id="stars" />
       <div id="stars2" />
       <div id="stars3" />
+      <Navigate to="/" />
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/website-sasikanduri" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/experience" element={<Experience />} />
         </Routes>
       </Router>
