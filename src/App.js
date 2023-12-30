7dd0b2d629179ee1f59c44013ef2fed46323786e
shashi <shashi.kanduri@outlook.com>
2023-12-30 18:26:07 +0530

    test

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/experience" element={<Experience />} />
         </Routes>
+        <Navigate to="/" />
       </Router>
     </div>
   );
