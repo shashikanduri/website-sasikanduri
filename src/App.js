78eef92aefffb09d243a15d8823654633d87e5a7
shashi <shashi.kanduri@outlook.com>
2023-12-30 18:05:25 +0530

    test

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/website-sasikanduri" element={<Home />} />
           <Route path="/experience" element={<Experience />} />
         </Routes>
       </Router>
